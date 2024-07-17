7d2e87cf2520d2d1f25e7f1ea20034642db669d2
mitex22 <mitex22@abv.bg>
2024-07-17 15:53:39 +0300

    Add Home component and React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/header/Header"
+import Home from "./components/home/Home"
 
 function App() {
 	return (
@@ -6,6 +7,8 @@ function App() {
 			<div id="box">
 
 				<Header />
+
+				<Home />
 
 			</div>
 		</>
