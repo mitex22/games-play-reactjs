26f1148be12bb54181700f87e4e100ab16fb515c
mitex22 <mitex22@abv.bg>
2024-07-18 14:38:10 +0300

    nest Routes in <main>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,16 +16,16 @@ function App() {
 
 				{/* <!-- Main Content --> */}
 				<main id="main-content">
-					
-				</main>
 
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/games" element={<GameList />} />
-					<Route path="/games/create" element={<GameCreate />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
+
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/games" element={<GameList />} />
+						<Route path="/games/create" element={<GameCreate />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+					</Routes>
+				</main>
 
 			</div>
 		</>
