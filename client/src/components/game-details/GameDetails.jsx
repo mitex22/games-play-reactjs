5725b6a0bce47d21d9177747f3c5838c85b0ce31
mitex22 <mitex22@abv.bg>
2024-07-25 19:02:46 +0300

    hide comment form when not authenticated

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -90,18 +90,20 @@ const GameDetails = () => {
 
             {/* <!-- Bonus --> */}
             {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-            <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form" onSubmit={commentSubmitHandler}>
-                    <textarea
-                        placeholder="Comment......"
-                        name="comment"
-                        onChange={commentInputHandler}
-                        value={comment}
-                    ></textarea>
-                    <input className="btn submit" type="submit" value="Add Comment" />
-                </form>
-            </article>
+            {userId && (
+                <article className="create-comment">
+                    <label>Add new comment:</label>
+                    <form className="form" onSubmit={commentSubmitHandler}>
+                        <textarea
+                            placeholder="Comment......"
+                            name="comment"
+                            onChange={commentInputHandler}
+                            value={comment}
+                        ></textarea>
+                        <input className="btn submit" type="submit" value="Add Comment" />
+                    </form>
+                </article>
+            )}
 
         </section>
     )
